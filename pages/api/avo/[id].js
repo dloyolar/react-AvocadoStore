ff90f3908ffd68ba5fc223d1ab6277816210b33d
dloyolar <dieegolr@gmail.com>
2022-02-13 13:22:11 -0300

    [ADD] new endpoint to get data by id

---
+++ pages/api/avo/[id].js
@@ -0,0 +1,8 @@
+import Database from '../../../database/db';
+
+const getAvoById = async (req, res) => {
+  const data = await Database.getById(req.query.id);
+  res.status(200).json(data);
+};
+
+export default getAvoById;
