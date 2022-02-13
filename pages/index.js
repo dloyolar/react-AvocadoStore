fff0074c9e19168747c8f50df1a0bc527c8e8738
dloyolar <dieegolr@gmail.com>
2022-02-13 13:23:51 -0300

    [WIP] test custom hook in landing to fetching data

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import Navbar from '../components/Navbar/Navbar';
+import useGetAvocados from '../hooks/useGetAvocados';
 
 const Home = () => {
+  const { products } = useGetAvocados();
+
   return (
     <div>
       <Navbar />
       <p>Home</p>
+      {products.map((product) => (
+        <div key={product.id}>{product.name}</div>
+      ))}
     </div>
   );
 };
---
+++ pages/product/[productId].js
@@ -0,0 +1,15 @@
+import { useRouter } from 'next/router';
+
+import useGetAvocadosById from '../../hooks/useGetAvocadosById';
+
+const ProductItem = () => {
+  const {
+    query: { productId },
+  } = useRouter();
+
+  const { product } = useGetAvocadosById(productId);
+
+  return <h1>ProductItem {product?.name}</h1>;
+};
+
+export default ProductItem;
